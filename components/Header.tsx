d866deb692ae4dac3fcd3c74383bca37fa4b1203
Md Ashrafujjaman Tutul <ashrafujjamantutul@gmail.com>
2025-05-14 16:39:22 +0600

    Add login and sign up pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,19 +8,19 @@ const Header = () => {
   const user = null;
   return (
     <header
-      className="relative h-24 w-full flex items-center justify-between bg-popover px-3 sm:px-8 py-0"
+      className="relative h-16 w-full flex items-center justify-between bg-popover px-3 sm:px-8"
       style={{ boxShadow: shadow }}
     >
       <Link href={"/"} className="flex items-center gap-2">
         <Image
           src={"/logo.png"}
-          height={60}
-          width={60}
+          height={50}
+          width={50}
           alt="logo"
           className="rounded-full"
           priority
         />
-        <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">
+        <h1 className="flex flex-col pb-1 text-xl font-semibold leading-6">
           NEXT <span>SUPA</span>
         </h1>
       </Link>
@@ -33,7 +33,7 @@ const Header = () => {
               <Link href={"/signup"} className="hidden sm:block">Sign up</Link>
             </Button>
             <Button asChild variant="outline">
-              <Link href={"/signin"}>Login</Link>
+              <Link href={"/login"}>Login</Link>
             </Button>
           </>
         )}
